8cd1d91a7cb98eed8eed8cec045e3f6ce8be9891
Erik de Jonge <erik@a8.nl>
2015-07-30 09:16:16 +0200

    Thursday 30 July 2015 (week:30 day:211), 09:16:13

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def main():
 
 
     setup(name='pycodequality',
-          version='36',
+          version='37',
           description='Pylint measures code quality for every file in the folder, returns an average',
           url='https://github.com/erikdejonge/pycodequality',
           author='Erik de Jonge',
